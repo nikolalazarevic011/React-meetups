fb1a6cd3ef30d48f92af843c1de2eec497326efc
Nikola Lazarevic <you@example.com>
2022-06-24 21:13:56 +0200

    added fetching data and react context

--- src/pages/AllMeetup.js
+++ src/pages/AllMeetup.js
@@ -1,29 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import MeetupList from "../components/meetups/MeetupList";
 
-
 function AllMeetupsPage() {
-
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
   const [LoadedMeetups, setLoadedMeetups] = useState([]);
 
+  useEffect(() => {
+    fetch(
+      "https://meetups-a0d13-default-rtdb.europe-west1.firebasedatabase.app/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
 
-  fetch(
-    "https://meetups-a0d13-default-rtdb.europe-west1.firebasedatabase.app/meetups.json"
-  ).then(response => {
-    return response.json()
-  }).then(data => {
-    setIsLoading(false)
-    setLoadedMeetups(data)
-  })
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
 
   if (isLoading) {
     return (
       <section>
         <p>Loading... </p>
       </section>
-    )
+    );
   }
 
   return (
